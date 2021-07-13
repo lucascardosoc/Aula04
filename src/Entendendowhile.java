598cc27b542ccf1e96e03de327becb2707ca6dfc
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-07-13 06:34:40 -0300

    Entendendo While

--- src/Entendendowhile.java
+++ src/Entendendowhile.java
@@ -3,13 +3,11 @@ public class Entendendowhile {
 
 	public static void main(String[] args) {
 		int a = 0;
-	    while(a < 100) {
-		System.out.println("Esse é o oi de número " + a);
-		a = a + 1;
+		while (a < 100) {
+			System.out.println("Esse é o oi de número " + a);
+			a = a + 1;
+		}
+
 	}
-	
-}
-	
-	
-	
+
 }
